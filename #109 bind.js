f26a47727abcfc813813da353bb2f02acb783c0f
cheer4chai <519644240@qq.com>
2018-03-06 00:00:50 +0800

    update

---
+++ #109 bind.js
@@ -0,0 +1,29 @@
+//实现一个new
+
+function objectFactory() {
+
+    var obj = new Object(),
+
+    Constructor = [].shift.call(arguments);
+
+    obj.__proto__ = Constructor.prototype;
+
+    var ret = Constructor.apply(obj, arguments);
+
+    return typeof ret === 'object' ? ret : obj;
+
+};
+
+//实现一个bind函数
+
+Function.prototype.bind2 = function(context) {
+    var _this = this;
+    var arg = [...context].slice(1);
+    var fBound = function() {
+        var bindArgs = Array.prototype.slice.call(arguments);
+        // 当作为构造函数时，this 指向实例，此时结果为 true，将绑定函数的 this 指向该实例，可以让实例获得来自绑定函数的值
+        // 以上面的是 demo 为例，如果改成 `this instanceof fBound ? null : context`，实例只是一个空对象，将 null 改成 this ，实例会具有 habit 属性
+        // 当作为普通函数时，this 指向 window，此时结果为 false，将绑定函数的 this 指向 context
+        return self.apply(this instanceof fBound ? this : context, args.concat(bindArgs));
+    }
+}
